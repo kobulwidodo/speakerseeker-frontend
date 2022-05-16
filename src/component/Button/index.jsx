88401ae4af4c161c07f67b0da9d7ba5649eb9dcc
kobulwidodo <fadhilhan01@gmail.com>
2022-05-16 20:26:46 +0700

    feat: add button component

---
+++ src/component/Button/index.jsx
@@ -0,0 +1,45 @@
+import classNames from "classnames";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Button = ({ label, type, icon, className, isLink, to }) => {
+  const classname = classNames(
+    "bg-[#7098E0] text-white text-lg font-normal py-2 px-7 rounded-full",
+    {
+      "bg-[#FFFFFF] border border-[#7098E0] text-[#7098E0] text-center":
+        type === "light",
+    },
+    className
+  );
+  return (
+    <>
+      {isLink ? (
+        <Link to={to} className={classname}>
+          {label}
+        </Link>
+      ) : (
+        <button
+          type="submit"
+          name="submit"
+          className={classNames(
+            "bg-[#7098E0] text-white text-lg font-normal py-2 px-7 rounded-full",
+            {
+              "bg-[#FFFFFF] border border-[#7098E0] text-[#7098E0] text-center":
+                type === "light",
+            },
+            className
+          )}
+        >
+          <div className={classNames("justify-center", { flex: icon })}>
+            {icon ? (
+              <img src={icon} className="mr-3" width="25" alt="" />
+            ) : null}
+            {label}
+          </div>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Button;
