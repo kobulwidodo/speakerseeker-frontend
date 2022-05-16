8050a1d4e52d7c3a41591ceae267b9ea212c37fa
kobulwidodo <fadhilhan01@gmail.com>
2022-05-16 20:27:15 +0700

    feat: init routers

---
+++ src/routers/index.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Login from "../pages/Login";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routers;
