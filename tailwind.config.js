9a567a749c0d399df65c01106c6fb8efe73c8370
kobulwidodo <fadhilhan01@gmail.com>
2022-05-16 20:22:36 +0700

    feat: init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      playfair_display: '"Playfair Display"',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
