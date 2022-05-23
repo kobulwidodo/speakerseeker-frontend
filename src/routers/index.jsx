c9048d625f8835a8cca6885a868fc4d185a925fc
kobulwidodo <fadhilhan01@gmail.com>
2022-05-23 21:19:50 +0700

    feat: implement routing group

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { UserWrapper } from "../context/UserContext";
+import BookSpeaker from "../pages/BookSpeaker";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Speakers from "../pages/Speakers";
 import SpeakersDetail from "../pages/SpeakersDetail";
+import AuthRoute from "./AuthRoute";
+import ProtectedRoute from "./ProtectedRoute";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -24,10 +27,15 @@ const Routers = () => {
       <UserWrapper>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route element={<AuthRoute />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
           <Route path="/speakers" element={<Speakers />} />
           <Route path="/speakers/:id/:name" element={<SpeakersDetail />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/speakers/:id/:name/book" element={<BookSpeaker />} />
+          </Route>
         </Routes>
       </UserWrapper>
     </BrowserRouter>
