3b190be08540f8c863a1bf0fd3a93369bcefad34
kobulwidodo <fadhilhan01@gmail.com>
2022-05-23 21:17:42 +0700

    feat: add auth routing

---
+++ src/routers/AuthRoute.js
@@ -0,0 +1,16 @@
+import { MailIcon } from "@heroicons/react/outline";
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
+
+const AuthRoute = () => {
+  const { isAuthenticated } = useUserContext();
+
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthRoute;
