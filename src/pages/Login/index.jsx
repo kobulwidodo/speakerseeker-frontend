4dde9162b73f706a1afed591cffbb5f78476b61d
kobulwidodo <fadhilhan01@gmail.com>
2022-05-24 22:16:59 +0700

    style: code clean up

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import image from "../../assets/images/pic_signin.svg";
 import google from "../../assets/images/logo_google.svg";
 import or from "../../assets/images/or.svg";
