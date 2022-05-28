8cd6c34412d83e9d27e6e5424499ca610a6b400c
kobulwidodo <fadhilhan01@gmail.com>
2022-05-28 20:00:45 +0700

    feat: add notification feature

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,9 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { Link, NavLink } from "react-router-dom";
 import userIcon from "../../assets/images/user_icon.svg";
+import notifIcon from "../../assets/images/notif_icon.svg";
 import iconNotif from "../../assets/images/carbon_notification-new.svg";
 import { useUserContext } from "../../context/UserContext";
 import Button from "../Button";
@@ -71,13 +71,56 @@ const Navbar = () => {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {isAuthenticated ? (
                   <>
-                    <button
-                      type="button"
-                      className="p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <img src={iconNotif} className="h-8 w-8" alt="" />
-                    </button>
+                    <Menu as="div" className="ml-3 relative">
+                      <div>
+                        <Menu.Button className="p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-white">
+                          <span className="sr-only">View notifications</span>
+                          <img src={iconNotif} className="h-8 w-8" alt="" />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-96 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <h1 className="px-4 py-2 font-medium text-lg border-b">
+                                Notification
+                              </h1>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Link
+                                to="/rating/1/vina-muliana"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-4 text-sm text-gray-700"
+                                )}
+                              >
+                                <div className="flex">
+                                  <img
+                                    src={notifIcon}
+                                    width={24}
+                                    height={24}
+                                    alt=""
+                                  />
+                                  <p className="ml-3">
+                                    Give a rating and review to Vina Muliana
+                                  </p>
+                                </div>
+                              </Link>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
                     <Menu as="div" className="ml-3 relative">
                       <div>
                         <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
